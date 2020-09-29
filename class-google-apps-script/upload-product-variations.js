ec989e3105dc7ba9d05fc8908468c64d41050cf6
Walace Silva <wsilva.emp@gmail.com>
2020-09-28 21:36:14 -0300

    [improvements] ID do Produto was added

--- class-google-apps-script/upload-product-variations.js
+++ class-google-apps-script/upload-product-variations.js
@@ -113,7 +113,7 @@ function uploadProduct(url, resource) {
  */
 function groupProductRows(rows) {
   result = rows.reduce(function(group, row) {
-    var rowValue = '' + row[0]; // Código do Produto
+    var rowValue = '' + row[1]; // (B) Código do Produto
     
     if (typeof group[rowValue] == 'undefined') {
       group[rowValue] = [];
@@ -135,8 +135,17 @@ function exportProducts(categoryId, sheet, values) {
     var preProduct = prepareProduct(categoryId, productGroup[productRef]);
     preProduct['category_id'] = categoryId;
     
-    // SpreadsheetApp.getUi().alert(JSON.stringify(preProduct));    
-    uploadProduct(URL_API + '/products/bulk', preProduct);
+    try {
+      var product = uploadProduct(URL_API + '/products/bulk', preProduct);
+
+      for(var i = 0; i < rowsWithoutHeader.length; i++) {
+        if (rowsWithoutHeader[i][1] === productRef) {
+          sheet.getRange('A' + (i + 2)).setValue(product.id);
+        }
+      }
+    } catch (e) {
+      SpreadsheetApp.getUi().alert(e);
+    }
   }
   
   SpreadsheetApp.getUi().alert('Operation finished');
@@ -147,39 +156,39 @@ function exportProducts(categoryId, sheet, values) {
  * @param {*} row 
  */
 function prepareProductVariantResource(row) {
-  var stock_quantity = row[12]; // (M) Estoque
+  var stock_quantity = row[13]; // (N) Estoque
 
   var images = [];
-  if ( typeof row[23] == 'string' ) {
-    var images = row[23].split(';'); // (X) Conjunto de Images
+  if ( typeof row[24] == 'string' ) {
+    var images = row[24].split(';'); // (Y) Conjunto de Images
   }
 
   var attribute_terms = [];
-  if ( (typeof row[10] == 'string' && row[10].length > 0) || typeof row[10] == 'number' ) {
+  if ( (typeof row[11] == 'string' && row[11].length > 0) || typeof row[11] == 'number' ) {
     attribute_terms.push({
       "type": "product_size",
-      "name": row[10], // (K) Tamanho
+      "name": row[11], // (L) Tamanho
     });
   }
 
-  if ( typeof row[11] == 'string' && row[11].length > 0 ) {
+  if ( typeof row[12] == 'string' && row[12].length > 0 ) {
     attribute_terms.push({
       "type": "product_color",
-      "name": row[11], // (L) Cor
+      "name": row[12], // (M) Cor
     });
   }
   
   return {
-	  "sku": row[1], // (B) Código Interno
+	  "sku": row[2], // (C) Código Interno
 	  "stock_quantity": stock_quantity,
     "stock_status": stock_quantity > 0 ? 'instock' : null,
-    "vendor_internal_code": row[1], // (B) Código Interno
-    "price": row[13], // (N) Preço
-    "weight": row[18], // (S) Peso
-    "depth": row[19], // (T) Comprimento
-    "width": row[20], // (U) Largura
-    "height": row[21], // (V) Altura
-    "permalink": row[1], // (B) Código Interno
+    "vendor_internal_code": row[2], // (C) Código Interno
+    "price": row[14], // (O) Preço
+    "weight": row[15], // (T) Peso
+    "depth": row[20], // (U) Comprimento
+    "width": row[21], // (V) Largura
+    "height": row[22], // (W) Altura
+    "permalink": row[2], // (C) Código Interno
     "attribute_terms": attribute_terms,
     "images": images,
     "image": images.length > 0 ? images[0] : null
@@ -195,26 +204,26 @@ function prepareProductResource(categoryId, row) {
   return {      
     "account_id": CLIENT_ID, // ID Conta
     "category_id": categoryId, // ID Categoria
-    "vendor_internal_code": row[1], // (B) Código Interno
-    "product_brands": row[2] ? row[2].split(',') : [], // (C) Marca da Moto
-    "product_models": row[3] ? row[3].split(',') : [], // (D) Modelo da Moto
-    "product_years": row[4] ? row[4].split(';') : [], // (E) Ano da Moto
-    "product_group": row[5], // (F) Grupo do Produto
-    "product_type": row[6], // (G) ID Item
-    "title": row[7], // (H) Nome do Produto
-    "short_description": row[8], // (I) Breve descrição
-    "description": row[9], // (J) Descrição
-    "price": row[13], // (N) Preço
-    "discount": row[14], // (O) % Desconto (Clube Veloce)
-    "product_used": row[15], // (P) Usado 1 /Novo 0
-    "accept_swap": row[16], // (Q) Aceita trocas? 0N - 1S
-    "has_installments": row[17], // (R) Aceita parcelamentos? 0N - 1S
-    "weight": row[18], // (S) Peso
-    "length": row[19], // (T) Comprimento
-    "width": row[20], // (U) Largura
-    "height": row[21], // (V) Altura
-    "product_wear": row[22], // (W) Desgaste
-    "images": row[23] ? row[23].split(';') : [] // (X) Conjunto de Images
+    "vendor_internal_code": row[2], // (C) Código Interno
+    "product_brands": row[3] ? row[3].split(',') : [], // (D) Marca da Moto
+    "product_models": row[4] ? row[4].split(',') : [], // (E) Modelo da Moto
+    "product_years": row[5] ? row[5].split(';') : [], // (F) Ano da Moto
+    "product_group": row[6], // (G) Grupo do Produto
+    "product_type": row[7], // (H) ID Item
+    "title": row[8], // (I) Nome do Produto
+    "short_description": row[9], // (J) Breve descrição
+    "description": row[10], // (K) Descrição
+    "price": row[14], // (O) Preço
+    "discount": row[15], // (P) % Desconto (Clube Veloce)
+    "product_used": row[16], // (Q) Usado 1 /Novo 0
+    "accept_swap": row[17], // (R) Aceita trocas? 0N - 1S
+    "has_installments": row[18], // (S) Aceita parcelamentos? 0N - 1S
+    "weight": row[19], // (T) Peso
+    "length": row[20], // (U) Comprimento
+    "width": row[21], // (V) Largura
+    "height": row[22], // (W) Altura
+    "product_wear": row[23], // (X) Desgaste
+    "images": row[24] ? row[24].split(';') : [] // (Y) Conjunto de Images
   }
 }
 
